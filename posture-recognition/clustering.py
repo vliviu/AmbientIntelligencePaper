b3916aeaaf38ca2162d244ab64adc1f81c1c26e9
Liviu Vladutu <liviu.vladutu@gmail.com>
2023-02-28 13:33:57 +0200

    Create clustering.py
    Added clustering.py

---
+++ posture-recognition/clustering.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as pp
+# For this to work, you need to upgrade threadpoolctl  to version >3. w smth like:
+#  pip install -Iv threadpoolctl==3.1.0
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+
+def cluster(features_vectors):
+    features_vectors = StandardScaler().fit_transform(features_vectors)
+    db = DBSCAN(eps=0.3, min_samples=10).fit(features_vectors)
+    core_samples = db.core_sample_indices_
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    
+#     print 'Clusters', n_clusters, 'labels', labels
+    
+    
+    pp.plot(labels)
+    pp.show()
+    
+    
+    
